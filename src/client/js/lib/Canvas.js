56847133fe4bfd27f0555ed444463d15b8810ac1
Daniel McQuiston <daniel.mcquiston@careerstep.com>
2017-03-10 15:42:08 -0700

    Add unknown changes

---
+++ src/client/js/lib/Canvas.js
@@ -0,0 +1,5 @@
+;(function(global){
+	function Canvas () {}
+
+	global.Canvas = Canvas
+})(window)
---
+++ src/server/lib/MovePacket.js
@@ -0,0 +1,11 @@
+"use strict"
+
+function MovePacket () {
+	let pos = {x, y, z}
+	let mov = {x, y, z}
+	let timestamp
+	let movMultiplier
+	let objId
+}
+
+module.exports = MovePacket
